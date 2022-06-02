5c7385b52fededd0497a67041bd330e97f189d5c
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-06-02 15:15:41 -0400

    added script funtionality for fetching exercise giph and some routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
+router.get("/exercise", (req, res) => {
+  res.render("giphy");
+});
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/previousworkouts", (req, res) => {
+  res.render("previousworkouts");
 });
 
 module.exports = router;
