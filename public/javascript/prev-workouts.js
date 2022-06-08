9330be0aac1aed58f84d7bd5c25e6156a796b6b3
palakdevgan <m7.palak@gmail.com>
2022-06-08 11:15:29 -0400

    fixed bootstrap issue

--- public/javascript/prev-workouts.js
+++ public/javascript/prev-workouts.js
@@ -1,8 +1,8 @@
-$('#fromDate').change(function() {
+$('#fromDate').change(function(){
     var fromDate = $('#fromDate').val();
     $('#toDate').prop("disabled", false);
     $("#toDate").attr("min", fromDate);
-});
+ });
 
 // hide prev workouts table on first page load
 $(".prev_workouts").hide();
@@ -13,15 +13,15 @@ async function newFormHandler(event) {
     const to_date = document.querySelector('input[name="to_date"]').value;
     var fromDate = moment(from_date).format('YYYY-MM-DD');
     var toDate = moment(to_date).format('YYYY-MM-DD');
-    // API request
+    //console.log(fromDate, toDate)
     const response = await fetch(`/api/workouts`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then((response) => response.json())
-        .then(function(data) {
-            // if data exists
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => response.json())
+        .then(function (data) {
+
             if (data.length > 0) {
 
                 // filter out data between from and to dates
@@ -30,7 +30,7 @@ async function newFormHandler(event) {
                     //console.log(date);
                     return (date >= fromDate && date <= toDate);
                 });
-
+                
                 // display filtered data if exists
                 if (filtered_data.length > 0) {
                     // show table 
@@ -46,7 +46,7 @@ async function newFormHandler(event) {
 }
 
 function setDataToTable(jsonData) {
-    // using datatable function to initaialize datatable with data
+
     $('#example').DataTable({
         pagination: "bootstrap",
         destroy: true,
@@ -56,8 +56,10 @@ function setDataToTable(jsonData) {
         // datatable columns
         "columns": [{
                 "data": "date",
-                type: 'date',
-                render: function(jsonData, type, row, meta) {
+                 type: 'date',
+                // render: $.fn.dataTable.render.moment('YYYY-MM-DDTHH:mm:ss.SSSSSSZ', 'YYYY-MM-DD')
+                //   render: $.fn.dataTable.render.moment().utc().format('YYYY-MM-DD')
+                render: function (jsonData, type, row, meta) {
                     return moment.utc(jsonData).format('YYYY-MM-DD');
                 }
             },
@@ -74,12 +76,13 @@ function setDataToTable(jsonData) {
         dom: 'Bfrtip',
         // export data buttons
         buttons: [
-            { extend: 'excelHtml5', className: 'previousbtn', text: 'Download Excel' },
-            { extend: 'csvHtml5', className: 'previousbtn', text: 'Download CSV' },
-            { extend: 'pdfHtml5', className: 'previousbtn', text: 'Download PDF' }
+            //{ extend: 'copyHtml5', className: 'previousbtn' ,text:'Copy'},
+            { extend: 'excelHtml5', className: 'previousbtn' ,text:'Download Excel'},
+            { extend: 'csvHtml5', className: 'previousbtn' ,text:'Download CSV'},
+            { extend: 'pdfHtml5', className: 'previousbtn' ,text:'Download PDF'}
         ]
-
+        
     });
-
+   
 }
 document.querySelector('.searchPrev').addEventListener('submit', newFormHandler);
